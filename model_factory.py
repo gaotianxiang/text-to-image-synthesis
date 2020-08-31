26b2e8c0dc79e43963abd5df63100afbcb88747e
Tianxiang Gao <gtx9726@gmail.com>
2020-08-31 18:39:38 -0400

    Move the interface to a single file.
    This commit moves the definition of model interface to a single file. More arguments are added to the get_model function.

--- model_factory.py
+++ model_factory.py
@@ -1,52 +1,24 @@
 """Returns model according to the configuration."""
 
-
-class Model:
-    """Model interfaces for all generative models.
-
-    Attributes:
-        dtst_name:
-        compression_size:
-        noise_size:
-        batch_size
-    """
-
-    def __init__(self, dtst_name, compression_size, noise_size, batch_size, use_condition):
-        """Initializes the object.
-
-        Args:
-            dtst_name: Dataset name.
-            compression_size: Size to which the embedding vectors will be projected to.
-            noise_size: Size of the noise vectors that are used to generate images.
-            batch_size:
-            use_condition:
-        """
-        self.dtst_name = dtst_name
-        self.compression_rate = compression_size
-        self.noise_size = noise_size
-        self.batch_size = batch_size
-        self.use_condition = use_condition
-
-    def train_step(self, inputs, loss_function, optimizer):
-        raise NotImplementedError
-
-    def generate(self, use_condition, ground_truth_img, embedding, caption, num, num_per_caption, visualize_tool,
-                 output_dir):
-        raise NotImplementedError
+from gan import GAN
 
 
-def get_models(model_name, dtst_name):
+def get_model(model_name, dtst_name, use_condition, batch_size, noise_size, compression_size):
     """Returns model according to model and dataset name.
 
     Args:
         model_name: Which type of model to use. Either gan, vae, or flow.
         dtst_name: Dataset name. Model structure could different for different model.
+        use_condition: Whether to conditioned generative model.
+        batch_size:
+        noise_size:
+        compression_size:
 
     Returns:
         A model.
     """
     if model_name == 'gan':
-        return _get_gan_model(dtst_name)
+        return GAN(dtst_name, compression_size, noise_size, batch_size, use_condition)
     # elif model_name == 'vae':
     #     return _get_vae_model(dtst_name)
     # elif model_name == 'flow':
---
+++ model_interface.py
@@ -0,0 +1,35 @@
+"""Model interfaces."""
+
+
+class Model:
+    """Model interfaces for all generative models.
+
+    Attributes:
+        dtst_name:
+        compression_size:
+        noise_size:
+        batch_size:
+        use_condition:
+    """
+
+    def __init__(self, dtst_name, compression_size, noise_size, batch_size, use_condition):
+        """Initializes the object.
+
+        Args:
+            dtst_name: Dataset name.
+            compression_size: Size to which the embedding vectors will be projected to.
+            noise_size: Size of the noise vectors that are used to generate images.
+            batch_size:
+            use_condition:
+        """
+        self.dtst_name = dtst_name
+        self.compression_rate = compression_size
+        self.noise_size = noise_size
+        self.batch_size = batch_size
+        self.use_condition = use_condition
+
+    def train_step(self, inputs, loss_function, optimizer):
+        raise NotImplementedError
+
+    def generate(self, embedding, num, num_per_caption):
+        raise NotImplementedError
